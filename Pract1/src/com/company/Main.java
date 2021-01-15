a48d7c893de52603927959d3c56b2313919874c4
AleshaMolodec <aleks.fandoren@gmail.com>
2021-01-15 15:39:30 +0300

    Initial commit

---
+++ Pract1/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Первое задание:\nHello java!!!\n");
+
+        double a = 3;
+        double b = 4;
+        double с;
+
+        с = Math.sqrt (a* a + b* b);
+        System.out.println ("Второе задание:\nc = "+ с);
+    }
+}
