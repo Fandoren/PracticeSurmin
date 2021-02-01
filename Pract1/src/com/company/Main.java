c218e8e8da9122baf58eabdd1c627557da931722
AleshaMolodec <aleks.fandoren@gmail.com>
2021-02-01 10:04:17 +0300

    Some changes, I don't know what

--- Pract1/src/com/company/Main.java
+++ Pract1/src/com/company/Main.java
@@ -6,9 +6,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        SortedSet<Integer> set = new TreeSet<>();
-        for (int i = 0; i < 10; i++)
-            set.add(i);
-        
+        System.out.println("Hello Java!");
     }
 }
