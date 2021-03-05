cdc6b849886ac7b9b31ffd5491ad2264adbea7d3
Fandoren <aleks.fandoren@gmail.com>
2021-03-06 02:00:29 +0300

    What is happening

--- Cipher1/src/com/company/Main.java
+++ Cipher1/src/com/company/Main.java
@@ -4,24 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 
+    private boolean paramA = true;
+    private boolean paramB = false;
+    private boolean paramC = true;
+
     public static void main(String[] args)
     {
-        String strcript = "", strdecript = "";
-        System.out.println("+------------------------> Шифр Цезаря <------------------------+");
-        Scanner input = new Scanner(System.in);
-        Scanner kayin = new Scanner(System.in);
-        System.out.print("Введите слово > ");
-        String str = input.nextLine();
-        for (int i = 0; i < str.length(); i++)
-        {
-            strcript += (char)(str.charAt(i) + 3);
-            //System.out.print((char)(str.charAt(i) + 3));
-        }
-        System.out.println("Закодированное сообщение >>  " + strcript + "  <<");
-        for (int j = 0; j < strcript.length(); j++)
-        {
-            strdecript += (char)(strcript.charAt(j) - 3);
-        }
-        System.out.println("Декодированное сообщение >>>  " + strdecript + "  <<<");
+
     }
+
 }
